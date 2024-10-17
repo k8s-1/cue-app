28f01ec1bfa5f9b9366f5b49d60ece0ac41a92da
pman <pman-$-432894328923@gmail.com>
2024-10-18 01:46:06 +0200

    ...

--- jq-update-script/dev.cue
+++ jq-update-script/dev.cue
@@ -1,7 +1,7 @@
 @if(dev)
 package dev
 {
-  "app1": "v9.0",
+  "app1": "v788.0",
   "app2": "v2.0",
   "app3": "v1.1"
 }
---
+++ jq-update-script/mod.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# usage: python mod.py app1 qa
+
+import json
+import sys
+
+app = str(sys.argv[1])
+env = str(sys.argv[2])
+
+# helper to split cue file into non-json and json
+def get_json(environment):
+    with open(environment + '.cue', 'r') as file:
+        content = file.read()
+        non_json, json_part = content.split('{', 1)
+        return non_json, json.loads('{' + json_part)
+
+
+# fetch version for the specified app and previous environment
+previous_environments = {
+    "qa":"dev",
+}
+_, data = get_json(previous_environments[env])
+version = data[app]
+
+# update version in the current environment's .cue file
+non_json, data = get_json(env)
+data[app] = version
+with open(env + '.cue', 'w') as file:
+    file.write(non_json_part + json.dumps(data, indent=2))
