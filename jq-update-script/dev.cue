0e16ce403f8df23cdc11608fc9e44e2b1bf49041
pman <pman-$-432894328923@gmail.com>
2024-10-18 00:17:28 +0200

    ...

---
+++ jq-update-script/dev.cue
@@ -0,0 +1,8 @@
+@if(dev)
+package dev
+
+{
+  "app1":"v1.0",
+  "app2":"v2.0",
+  "app3":"v1.1"
+}
---
+++ jq-update-script/modify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+NON_JSON=$(sed '/{/Q' dev.cue)
+JSON=$(sed -n '/{/,/}/p' dev.cue)
+NEW_VERSION="v9.0"
+
+NEW_JSON=$(echo "$JSON" | jq --arg new_version "$NEW_VERSION" '.app1 = $new_version')
+
+{
+    echo "$NON_JSON"
+    echo "$NEW_JSON"
+} > dev.cue.test
