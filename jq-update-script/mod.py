f7e29585363adcdae619a20c64deded1fea6c04c
pman <pman-$-432894328923@gmail.com>
2024-10-18 02:12:43 +0200

    chore: update file(s): mod.py

--- jq-update-script/mod.py
+++ jq-update-script/mod.py
@@ -6,15 +6,16 @@ import json
 import sys
 
 
-def get_json(environment):
+def get_json(environment: str) -> tuple[str, dict]:
     """Split file into non-JSON and JSON components."""
     with open(environment + '.cue', 'r') as file:
         content = file.read()
-        non_json, json_part = content.split('{', 1)
-        return non_json, json.loads('{' + json_part)
+
+    non_json, json_part = content.split('{', 1)
+    return non_json, json.loads('{' + json_part)
 
 
-if __name__ == "__main__":
+def main():
     app = sys.argv[1]  # Application name
     env = sys.argv[2]  # Environment name
 
@@ -28,5 +29,10 @@ if __name__ == "__main__":
     # update version in the current environment's .cue file
     non_json, data = get_json(env)
     data[app] = version
+
     with open(env + '.cue', 'w') as file:
         file.write(non_json + json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    main()
