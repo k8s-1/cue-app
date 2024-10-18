c7585f5a9d4d4cbe652decdfca9f164dfee2225c
pman <pman-$-432894328923@gmail.com>
2024-10-18 02:01:17 +0200

    chore: update file(s): mod.py

--- jq-update-script/mod.py
+++ jq-update-script/mod.py
@@ -5,8 +5,6 @@
 import json
 import sys
 
-app = sys.argv[1]
-env = sys.argv[2]
 
 def get_json(environment):
     """Split file into non-JSON and JSON components."""
@@ -15,15 +13,20 @@ def get_json(environment):
         non_json, json_part = content.split('{', 1)
         return non_json, json.loads('{' + json_part)
 
-# fetch version for the specified app and previous environment
-previous_environments = {
-    "qa":"dev",
-}
-_, data = get_json(previous_environments[env])
-version = data[app]
 
-# update version in the current environment's .cue file
-non_json, data = get_json(env)
-data[app] = version
-with open(env + '.cue', 'w') as file:
-    file.write(non_json + json.dumps(data, indent=2))
+if __name__ == "__main__":
+    app = sys.argv[1]  # Application name
+    env = sys.argv[2]  # Environment name
+
+    # fetch version for the specified app and previous environment
+    previous_environments = {
+        "qa": "dev",
+    }
+    _, data = get_json(previous_environments[env])
+    version = data[app]
+
+    # update version in the current environment's .cue file
+    non_json, data = get_json(env)
+    data[app] = version
+    with open(env + '.cue', 'w') as file:
+        file.write(non_json + json.dumps(data, indent=2))
